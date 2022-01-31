3e75fff4f732a40c6b92bd0ebe4bfd29854bf0d5
Joel Pryde <joelp@unity3d.com>
2022-01-31 22:05:45 +0100

    Basic token parsing

--- compiler.c
+++ compiler.c
@@ -1,7 +1,6 @@
 //
 // Created by joelpryde on 1/31/2022.
 //
-
 #include <stdio.h>
 #include "compiler.h"
 #include "scanner.h"
@@ -9,5 +8,22 @@
 void compile(const char* source)
 {
     initScanner(source);
+    int line = -1;
+    for (;;)
+    {
+        Token token = scanToken();
+        if (token.line != line)
+        {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else
+            printf("    |");
+
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF)
+            break;
+    }
 }
 

a07eb536c0da80f873e1d83b8f9b0a1efef2476e
Joel Pryde <joelp@unity3d.com>
2022-02-07 20:08:55 +0100

    WIP on hash table

--- value.h
+++ value.h
@@ -39,7 +39,7 @@ typedef struct
 #define AS_OBJ(value)       ((value).as.obj)
 
 #define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL(value)      ((Value){VAL_NIL, {.boolean = 0}})
+#define NIL_VAL()           ((Value){VAL_NIL, {.boolean = 0}})
 #define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(value)      ((Value){VAL_OBJ, {.obj = (Obj*)value}})
 

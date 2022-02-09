abffd32959ff2eb38c5f1a69fd209b63b61c3213
Joel Pryde <joelp@unity3d.com>
2022-02-09 11:10:42 +0100

    Basic statements/print

--- compiler.c
+++ compiler.c
@@ -101,6 +101,19 @@ static void consume(TokenType type, const char* message)
     errorAtCurrent(message);
 }
 
+static bool check(TokenType type)
+{
+    return parser.current.type == type;
+}
+
+static bool match(TokenType type)
+{
+    if (!check(type))
+        return false;
+    advance();
+    return true;
+}
+
 static void emitByte(uint8_t byte)
 {
     writeChunk(currentChunk(), byte, parser.previous.line);
@@ -210,6 +223,26 @@ static void expression()
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void printStatement()
+{
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+    emitByte(OP_PRINT);
+}
+
+static void statement()
+{
+    if (match(TOKEN_PRINT))
+    {
+        printStatement();
+    }
+}
+
+static void declaration()
+{
+    statement();
+}
+
 static void grouping()
 {
     expression();
@@ -305,8 +338,12 @@ bool compile(const char* source, Chunk* chunk)
     parser.panicMode = false;
 
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expected end of expression");
+
+    while (!match(TOKEN_EOF))
+    {
+        declaration();
+    }
+
     endCompiler();
     return !parser.hadError;
 }
--- vm.c
+++ vm.c
@@ -169,12 +169,14 @@ static InterpretResult run()
                 }
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
+            case OP_PRINT:
+                printValue(pop());
+                printf("\n");
+                break;
             case OP_RETURN:
-            {
                 printValue(pop());
                 printf("\n");
                 return INTERPRET_OK;
-            }
         }
     }
     return INTERPRET_RUNTIME_ERROR;

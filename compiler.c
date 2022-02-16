4b4654864e27dad46387b39d3699a4dddb248a1c
Joel Pryde <joelp@unity3d.com>
2022-02-16 20:50:07 +0100

    while statements

--- compiler.c
+++ compiler.c
@@ -139,6 +139,18 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
     emitByte(byte2);
 }
 
+static void emitLoop(int loopStart)
+{
+    emitByte(OP_LOOP);
+
+    int offset = currentChunk()->count - loopStart + 2;
+    if (offset >UINT16_MAX)
+        error("Loop body too large.");
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
 static int emitJump(uint8_t instruction)
 {
     emitByte(instruction);
@@ -284,6 +296,23 @@ static void printStatement()
     emitByte(OP_PRINT);
 }
 
+static void whileStatement()
+{
+    int loopStart = currentChunk()->count;
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
+}
+
 static void expressionStatement()
 {
     expression();
@@ -334,6 +363,8 @@ static void statement()
         printStatement();
     else if (match(TOKEN_IF))
         ifstatement();
+    else if (match(TOKEN_WHILE))
+        whileStatement();
     else if (match(TOKEN_LEFT_BRACE))
     {
         beginScope();

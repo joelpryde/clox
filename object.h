fefc178436a17e691d8b1b97ae889cb418482387
Joel Pryde <joelpryde@gmail.com>
2022-02-17 09:15:32 +0100

    Function objects

--- object.h
+++ object.h
@@ -6,17 +6,21 @@
 #define CLOX_OBJECT_H
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)     (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)  isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value)    isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value)  ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)   (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum
 {
+    OBJ_FUNCTION,
     OBJ_STRING
 } ObjType;
 
@@ -26,6 +30,14 @@ struct Obj
     struct Obj* next;
 };
 
+typedef struct
+{
+    Obj obj;
+    int arity;
+    Chunk chunk;
+    ObjString* name;
+} ObjFunction;
+
 struct ObjString
 {
     Obj obj;
@@ -34,6 +46,7 @@ struct ObjString
     uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(const char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);

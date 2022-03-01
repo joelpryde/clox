aef4fa6aae4ea4f55f5c0f7c5ea1b86b498f2a76
Joel Pryde <joelpryde@gmail.com>
2022-03-01 21:50:06 +0100

    Upvalues WIP

--- debug.c
+++ debug.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "debug.h"
+#include "object.h"
 
 void disassembleChunk(Chunk *chunk, const char *name)
 {
@@ -75,6 +76,10 @@ int disassembleInstruction(Chunk *chunk, int offset)
             return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
         case OP_SET_GLOBAL:
             return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+        case OP_GET_UPVALUE:
+            return constantInstruction("OP_GET_UPVALUE", chunk, offset);
+        case OP_SET_UPVALUE:
+            return constantInstruction("OP_SET_GLOBAL", chunk, offset);
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:
@@ -108,6 +113,15 @@ int disassembleInstruction(Chunk *chunk, int offset)
             printf("%-16s %4d ", "OP_CLOSURE", constant);
             printValue(chunk->constants.values[constant], false);
             printf("\n");
+
+            ObjFunction* function = AS_FUNCTION(chunk->constants.values[constant]);
+            for (int j = 0; j < function->upvalueCount; j++)
+            {
+                int isLocal = chunk->code[offset++];
+                int index = chunk->code[offset++];
+                printf("%04d    |               %s %d\n", offset - 2, isLocal ? "local" : "upvalue", index);
+            }
+
             return offset;
         }
         case OP_RETURN:
--- object.h
+++ object.h
@@ -40,6 +40,7 @@ typedef struct
 {
     Obj obj;
     int arity;
+    int upvalueCount;
     Chunk chunk;
     ObjString* name;
 } ObjFunction;

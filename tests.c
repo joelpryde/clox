993203b2077c131430bc690abdcab1ddf82708e4
Joel Pryde <joelpryde@gmail.com>
2022-03-15 22:47:08 +0100

    Clean up tests to use fixture

--- tests.c
+++ tests.c
@@ -24,70 +24,62 @@ static void reset()
     setupRun = true;
 }
 
-bool checkPrintOutput(const char* checkStr)
-{
-    return (strncmp(testPrintOutput, checkStr, strlen(checkStr)) == 0);
+struct TestFixture {
+    const char* expected;
+    InterpretResult result;
+};
+
+UTEST_F_SETUP(TestFixture) {
+    reset();
 }
 
-UTEST(interpret, print) {
-    reset();
-    InterpretResult result = interpret("print 1;");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("1"));
+UTEST_F_TEARDOWN(TestFixture) {
+    ASSERT_TRUE(utest_fixture->result == INTERPRET_OK);
+    ASSERT_TRUE(strncmp(testPrintOutput, utest_fixture->expected, strlen(utest_fixture->expected)) == 0);
 }
 
-UTEST(interpret, concat_strings) {
-    reset();
-    InterpretResult result = interpret("print \"jaba\" + \"hut\";");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("jabahut"));
+UTEST_F(TestFixture, interpret_print) {
+    utest_fixture->result = interpret("print 1;");
+    utest_fixture->expected = "1";
 }
 
-UTEST(interpret, while_) {
-    reset();
-    InterpretResult result = interpret("var a=0; while (a<3) a = a + 1; print a;");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("3"));
+UTEST_F(TestFixture, interpret_concat_strings) {
+    utest_fixture->result = interpret("print \"jaba\" + \"hut\";");
+    utest_fixture->expected = "jabahut";
 }
 
-UTEST(interpret, for_) {
-    reset();
-    InterpretResult result = interpret("var a=0; for (var t = 3; t > 0; t = t - 1) a = a + 1; print a;");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("3"));
+UTEST_F(TestFixture, interpret_while_) {
+    utest_fixture->result = interpret("var a=0; while (a<3) a = a + 1; print a;");
+    utest_fixture->expected = "3";
 }
 
-UTEST(interpret, fun_declaration) {
-    reset();
-    InterpretResult result = interpret("fun someFunction() { print \"blah\"; } print someFunction;");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("<fn someFunction>"));
+UTEST_F(TestFixture, interpret_for_) {
+    utest_fixture->result = interpret("var a=0; for (var t = 3; t > 0; t = t - 1) a = a + 1; print a;");
+    utest_fixture->expected = "3";
 }
 
-UTEST(interpret, fun_call) {
-    reset();
-    InterpretResult result = interpret("fun someFunction() { print \"blah\"; } someFunction();");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("blah"));
+UTEST_F(TestFixture, interpret_fun_declaration) {
+    utest_fixture->result = interpret("fun someFunction() { print \"blah\"; } print someFunction;");
+    utest_fixture->expected = "<fn someFunction>";
 }
 
-UTEST(interpret, fun_return) {
-    reset();
-    InterpretResult result = interpret("fun someFunction() { return \"blah\"; } print someFunction();");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("blah"));
+UTEST_F(TestFixture, interpret_fun_call) {
+    utest_fixture->result = interpret("fun someFunction() { print \"blah\"; } someFunction();");
+    utest_fixture->expected = "blah";
 }
 
-UTEST(interpret, native_clock) {
-    reset();
-    InterpretResult result = interpret("print clock();");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("0"));
+UTEST_F(TestFixture, interpret_fun_return) {
+    utest_fixture->result = interpret("fun someFunction() { return \"blah\"; } print someFunction();");
+    utest_fixture->expected = "blah";
 }
 
-UTEST(interpret, upvalues) {
-    reset();
-    InterpretResult result = interpret(
+UTEST_F(TestFixture, interpret_native_clock) {
+    utest_fixture->result = interpret("print clock();");
+    utest_fixture->expected = "0";
+}
+
+UTEST_F(TestFixture, interpret_upvalues) {
+    utest_fixture->result = interpret(
 "fun outer() {"
     "var x = \"outside\";"
     "fun inner() {"
@@ -96,13 +88,11 @@ UTEST(interpret, upvalues) {
     "inner();"
 "}"
 "outer();");
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("outside"));
+    utest_fixture->expected = "outside";
 }
 
-UTEST(interpret, closed_upvalues) {
-    reset();
-    InterpretResult result = interpret(
+UTEST_F(TestFixture, interpret_closed_upvalues) {
+    utest_fixture->result = interpret(
 "var f;"
 "fun foo(param) {"
     "fun f_() {"
@@ -113,6 +103,5 @@ UTEST(interpret, closed_upvalues) {
 "foo(\"param\");"
 "f();"
     );
-    ASSERT_TRUE(result == INTERPRET_OK);
-    ASSERT_TRUE(checkPrintOutput("param"));
+    utest_fixture->expected = "param";
 }
